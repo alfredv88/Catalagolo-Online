618d936d4c264c41bf2b0c1037c95af8d71b38d8
kevworka@gmail.com <kevworka@gmail.com>
2025-09-25 08:34:35 -0400

    fix: Remover dependencia cors del server.js para Railway

--- server.js
+++ server.js
@@ -7,14 +7,12 @@
  */
 
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
